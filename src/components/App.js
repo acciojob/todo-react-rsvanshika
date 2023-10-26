2ec417e45cd68e03fd87876162de01d31809c998
rsvanshika <vrsharma042@gmail.com>
2023-10-26 16:40:51 +0530

    submitting code-Thu Oct 26 2023 16:40:50 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,50 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+// import { defaultsDeep } from "cypress/types/lodash";
 
 const App = () => {
+
+  const [todo,setTodo]=useState("")
+  const [todolist,setTodoList]=useState([])
+
+  function implementAddTodo(e){
+    e.preventDefault();
+    setTodoList([...todolist,todo])
+    setTodo(" ");
+
+  }
+
+  function handleDelete(i){
+    setTodoList(todolist.filter((value,index)=>index!==i))
+
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={implementAddTodo}>
+      <input type="text" placeholder="Enter task"
+      onChange={(e)=>setTodo(e.target.value)} value={todo}/>
+      <button type="submit">Add Todo</button>
+      </form>
+
+    { 
+     todolist.map((item,index) =>(
+        <div>
+          <ul>
+            <li>
+              {item}
+              <button onClick={()=>handleDelete(index)} >Delete</button>
+            </li>
+          </ul>
+        </div>
+      ))
+    }
+    
+
+  
+      
+      
     </div>
   )
 }
